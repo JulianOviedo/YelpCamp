4d7884bd28850d5b3bef45d162475262afca45cc
JulianOviedo <julianovie234@gmail.com>
2023-02-20 18:56:29 -0300

    remove dotenv

--- yelp-front/src/pages/index.js
+++ yelp-front/src/pages/index.js
@@ -1,8 +1,6 @@
 import { useEffect } from "react"
 
 const BASE_URL = 'http://localhost:5000'
-const test = process.env.BASE_URL
-console.log(test)
 
 export default function Home() {
   useEffect(() => {
