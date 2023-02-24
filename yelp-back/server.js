7ad102d0547700ec910ae5f1982a73dbe2a34d45
JulianOviedo <julianovie234@gmail.com>
2023-02-24 16:23:20 -0300

    set mongoose

--- yelp-back/server.js
+++ yelp-back/server.js
@@ -1,6 +1,17 @@
 import express from 'express'
 import cors from 'cors'
 
+import mongoose from 'mongoose'
+import Campground from './models/campground.js';
+
+mongoose.connect('mongodb://localhost:27017/nombre-de-tu-base-de-datos', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('DATABASE IS CONNECTED'))
+  .catch(error => console.error('ERROR TRYING TO CONNECT MONGOOSE TO DATABASE', error));
+
+
+
+///------****  EXPRESS  ***--------------////
+
 const app = express()
 
 app.use(cors())
@@ -9,6 +20,8 @@ app.listen(5000, (req, res) => {
     console.log('server listen on port 5000')
 })
 
-app.get('/api',  (req, res) => {
-    res.send('testing port')
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({title:'my first campground'})
+    await camp.save()
+    res.send(camp)
 })
