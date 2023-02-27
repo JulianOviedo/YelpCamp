7fc8e0c30ba1db88fab3480e0bcbce8cfec3a1b2
JulianOviedo <julianovie234@gmail.com>
2023-02-27 11:00:22 -0300

    add seeder to work on dev

---
+++ yelp-back/seeds/index.js
@@ -0,0 +1,25 @@
+import {places, description} from './seedHelpers.js'
+import cities from './cities.js'
+
+import mongoose from 'mongoose'
+import Campground from '../models/campground.js';
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('SEEDER HAS BEEN EXECUTED'))
+  .catch(error => console.error('ERROR TRYING TO CONNECT MONGOOSE TO DATABASE', error));
+
+const sample = (arr) => {arr[Math.floor(Math.random() * arr.length)]}
+
+const seeder = async () => {
+    await Campground.deleteMany()
+    for (let i=0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000)
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(description)} ${sample(places)}`
+        })
+        await camp.save()
+    }
+}
+
+seeder();
--- yelp-back/server.js
+++ yelp-back/server.js
@@ -10,7 +10,7 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true,
 
 
 
-///------****  EXPRESS  ***--------------////
+///------****  EXPRESS  ***--------////
 
 const app = express()
 
