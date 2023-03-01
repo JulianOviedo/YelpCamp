7a26b36410283612d3979d440fbd0b766b9d7ed9
JulianOviedo <julianovie234@gmail.com>
2023-03-01 15:18:34 -0300

    styles and components

--- yelp-front/src/components/MainLayout.js
+++ yelp-front/src/components/MainLayout.js
@@ -1,10 +1,16 @@
+import Footer from './Footer'
 import NavBar from './NavBar'
 
 export default function MainLayout({ children }) {
     return (
         <>
-            <NavBar/>
-            {children}
+            <div className='d-flex flex-column vh-100'>
+                <NavBar/>
+                <main className='container mt-5'>
+                    {children}
+                </main>
+                <Footer/>
+            </div>
         </>
     )
 }
--- yelp-front/src/components/NavBar.js
+++ yelp-front/src/components/NavBar.js
@@ -1,6 +1,6 @@
 export default function NavBar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
                 <a className="navbar-brand" href="#">Navbar</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,17 +9,17 @@ export default function NavBar() {
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link " href="/">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Features</a>
+                            <a className="nav-link" href="/campgrounds/new">New campground</a>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Pricing</a>
+                        {/* <li className="nav-item">
+                            <a className="nav-link" href="/">Pricing</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
-                        </li>
+                        </li> */}
                     </ul>
                 </div>
             </div>
