bd0d5a50ebe2610f6d6cd620a9d4307a86b480f6
JulianOviedo <julianovie234@gmail.com>
2023-03-01 20:55:07 -0300

    js form boots now works

--- yelp-front/src/components/NavBar.js
+++ yelp-front/src/components/NavBar.js
@@ -14,12 +14,6 @@ export default function NavBar() {
                         <li className="nav-item">
                             <a className="nav-link" href="/campgrounds/new">New campground</a>
                         </li>
-                        {/* <li className="nav-item">
-                            <a className="nav-link" href="/">Pricing</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
-                        </li> */}
                     </ul>
                 </div>
             </div>
