03f357b6ef8012344a503d86378ee3c16862fb41
JulianOviedo <julianovie234@gmail.com>
2023-03-01 21:06:57 -0300

    add styles for newCampground and also component for the form

--- yelp-front/src/pages/campgrounds/new.js
+++ yelp-front/src/pages/campgrounds/new.js
@@ -1,21 +1,9 @@
-import Link from 'next/link'
+import NewCampgroundForm from '@/components/NewCampgroundForm'
 
 export default function NewCampground() {
-    const BASE_URL = 'http://localhost:5000'
-
     return (
         <>
-            <h1>New Campground</h1>
-            <form action={`${BASE_URL}/campgrounds`} method="POST">
-                <label htmlFor='title'> Title
-                    <input type='text' id="title" name='campground[title]'></input>
-                </label>
-                <label htmlFor='location'> Location
-                    <input type='text' id="location" name='campground[location]'></input>
-                </label>
-                <button type="submit">Add Campground</button>
-            </form>
-            <Link href='/'>Back to Home</Link>
+            <NewCampgroundForm/>
         </>
     )
 }
