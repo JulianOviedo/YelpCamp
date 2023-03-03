43889d6bc479f1972cc42d422053975ec2ffaf01
JulianOviedo <julianovie234@gmail.com>
2023-03-03 18:05:06 -0300

    ref navBar

--- yelp-front/src/components/NavBar.js
+++ yelp-front/src/components/NavBar.js
@@ -2,7 +2,6 @@ export default function NavBar() {
     return (
         <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Navbar</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
