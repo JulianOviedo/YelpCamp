7e0e67675f49e44a6d7227bd74c9917d08a817da
JulianOviedo <julianovie234@gmail.com>
2023-03-03 14:03:23 -0300

    ref editCmpgroundform component

--- yelp-front/src/components/EditCampgroundForm.js
+++ yelp-front/src/components/EditCampgroundForm.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 
-export default function EditCampgroundForm ({ id, title, location, imageUrl, price, description }) {
+export default function EditCampgroundForm ({ id, title, location, imageUrl, price, description, onChange }) {
     const BASE_URL = 'http://localhost:5000'
 
     return (
@@ -11,31 +11,31 @@ export default function EditCampgroundForm ({ id, title, location, imageUrl, pri
                     <div className="mb-3">
                         <label className="form-label" htmlFor="title">Title</label>
                         <input className="form-control" type="text" id="title" name="campground[title]"
-                            value={title}/>
+                            value={title} onChange={onChange}/>
                     </div>
                     <div className="mb-3">
                         <label className="form-label" htmlFor="location">Location</label>
                         <input className="form-control" type="text" id="location" name="campground[location]"
-                            value={location}/>
+                            value={location} onChange={onChange}/>
                     </div>
                     <div className="mb-3">
                         <label className="form-label" htmlFor="image">Image Url</label>
                         <input className="form-control" type="text" id="image" name="campground[image]"
-                            value={imageUrl}/>
+                            value={imageUrl} onChange={onChange}/>
                     </div>
                     <div className="mb-3">
                         <label className="form-label" htmlFor="price">Campground Price</label>
                         <div className="input-group">
                             <span className="input-group-text" id="price-label">$</span>
                             <input type="text" className="form-control" id="price" placeholder="0.00" aria-label="price"
-                                aria-describedby="price-label" name="campground[price]" value={price}/>
+                                aria-describedby="price-label" name="campground[price]" value={price} onChange={onChange}/>
                         </div>
                     </div>
 
                     <div className="mb-3">
                         <label className="form-label" htmlFor="description">Description</label>
                         <textarea className="form-control" type="text" id="description"
-                            name="campground[description]" value={description}></textarea>
+                            name="campground[description]" value={description} onChange={onChange}></textarea>
                     </div>
                     <div className="mb-3">
                         <button className="btn btn-info">Update Campground</button>
