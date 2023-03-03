c23dce61fd985525b236441811525cb0689d16c7
JulianOviedo <julianovie234@gmail.com>
2023-03-03 17:39:12 -0300

    ref show campground

--- yelp-front/src/pages/campgrounds/[_id]/edit.js
+++ yelp-front/src/pages/campgrounds/[_id]/edit.js
@@ -1,10 +1,9 @@
+import EditCampgroundForm from '@/components/EditCampgroundForm'
 import axios from 'axios'
-import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
 export default function EditCampground({ _id }) {
     const [campground, setCampground] = useState({})
-    const BASE_URL = 'http://localhost:5000'
 
     useEffect(() => {
         const fetchCampgroundDetails = async () => {
@@ -24,47 +23,15 @@ export default function EditCampground({ _id }) {
 
     return (
         <>
-            <div className="row">
-                <h1 className="text-center">Edit Campground</h1>
-                <div className="col-6 offset-3">
-                    <form action={`${BASE_URL}/campgrounds/${campground._id}?_method=PUT`} method="POST">
-                        <div className="mb-3">
-                            <label className="form-label" htmlFor="title">Title</label>
-                            <input className="form-control" type="text" id="title" name="campground[title]"
-                                value={campground.title} onChange={handleChange} />
-                        </div>
-                        <div className="mb-3">
-                            <label className="form-label" htmlFor="location">Location</label>
-                            <input className="form-control" type="text" id="location" name="campground[location]"
-                                value={campground.location} onChange={handleChange}/>
-                        </div>
-                        <div className="mb-3">
-                            <label className="form-label" htmlFor="image">Image Url</label>
-                            <input className="form-control" type="text" id="image" name="campground[image]"
-                                value={campground.image} onChange={handleChange}/>
-                        </div>
-                        <div className="mb-3">
-                            <label className="form-label" htmlFor="price">Campground Price</label>
-                            <div className="input-group">
-                                <span className="input-group-text" id="price-label">$</span>
-                                <input type="text" className="form-control" id="price" placeholder="0.00" aria-label="price"
-                                    aria-describedby="price-label" name="campground[price]" value={campground.price} onChange={handleChange}/>
-                            </div>
-                        </div>
-
-                        <div className="mb-3">
-                            <label className="form-label" htmlFor="description">Description</label>
-                            <textarea className="form-control" type="text" id="description"
-                                name="campground[description]" value={campground.description} onChange={handleChange}></textarea>
-                        </div>
-                        <div className="mb-3">
-                            <button className="btn btn-info">Update Campground</button>
-                        </div>
-                    </form>
-                    <Link href={`/campgrounds/${campground._id}`}>Go back</Link>
-
-                </div>
-            </div>
+            <EditCampgroundForm
+                id={campground._id}
+                imageUrl={campground.image}
+                title={campground.title}
+                description={campground.description}
+                location={campground.location}
+                price={campground.price}
+                onChange={handleChange}
+            />
         </>
     )
 }
