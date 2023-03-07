d7701a569f67fe9049058ebc2a7798a8c373a864
JulianOviedo <julianovie234@gmail.com>
2023-03-06 22:00:01 -0300

    form validation on bootstrap

--- yelp-front/src/components/NewCampgroundForm.js
+++ yelp-front/src/components/NewCampgroundForm.js
@@ -7,31 +7,31 @@ export default function NewCampgroundForm () {
         <div className="row">
             <h1 className="text-center">New Campground</h1>
             <div className="col-6 offset-3">
-                <form action={`${BASE_URL}/campgrounds`} method="POST">
+                <form action={`${BASE_URL}/campgrounds`} method="POST" noValidate>
                     <div className="mb-3">
                         <label className="form-label" htmlFor="title">Title</label>
-                        <input className="form-control" type="text" id="title" name="campground[title]"/>
+                        <input className="form-control" type="text" id="title" name="campground[title]" required />
                     </div>
                     <div className="mb-3">
                         <label className="form-label" htmlFor="location">Location</label>
-                        <input className="form-control" type="text" id="location" name="campground[location]"/>
+                        <input className="form-control" type="text" id="location" name="campground[location]" required />
                     </div>
                     <div className="mb-3">
                         <label className="form-label" htmlFor="image">Image Url</label>
-                        <input className="form-control" type="text" id="image" name="campground[image]"/>
+                        <input className="form-control" type="text" id="image" name="campground[image]" required />
                     </div>
                     <div className="mb-3">
                         <label className="form-label" htmlFor="price">Campground Price</label>
                         <div className="input-group">
                             <span className="input-group-text" id="price-label">$</span>
                             <input type="text" className="form-control" id="price" placeholder="0.00" aria-label="price"
-                                aria-describedby="price-label" name="campground[price]"/>
+                                aria-describedby="price-label" name="campground[price]" required />
                         </div>
                     </div>
 
                     <div className="mb-3">
                         <label className="form-label" htmlFor="description">Description</label>
-                        <textarea className="form-control" type="text" id="description" name="campground[description]"/>
+                        <textarea className="form-control" type="text" id="description" name="campground[description]" required />
                     </div>
                     <div className="mb-3">
                         <button className="btn btn-success">Add Campground</button>
@@ -40,5 +40,6 @@ export default function NewCampgroundForm () {
                 <Link href='/'>Back to Home</Link>
             </div>
         </div>
+
     )
 }
