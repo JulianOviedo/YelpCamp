e885c8dc731fb79d98a3acdff91b4096bc7e238c
JulianOviedo <julianovie234@gmail.com>
2023-03-10 13:55:16 -0300

    rendering reviews

--- yelp-front/src/pages/campgrounds/[_id].js
+++ yelp-front/src/pages/campgrounds/[_id].js
@@ -39,27 +39,40 @@ export default function ShowCampground({ _id }) {
                             </form>
                         </div>
                         <div className="card-footer text-muted">
-                                2 days ago
+                            2 days ago
                         </div>
                     </div>
                 </div>
             )}
             <div className="col-6">
                 <h2>Leave a Review</h2>
-                <form action={`${BASE_URL}/campgrounds/${_id}/reviews`} method="POST" ref={formRef} className={isValidated ? 'was-validated' : 'form-validate' + 'mb-3'} noValidate>
+                <form action={`${BASE_URL}/campgrounds/${_id}/reviews`} method="POST" ref={formRef} className={isValidated ? 'was-validated' : 'form-validate mb-3'} noValidate>
                     <div className="mb-3">
                         <label className="form-label" htmlFor="rating">Rating</label>
-                        <input className="form-range" type="range" min="1" max="5" name="review[rating]" id="rating" required/>
+                        <input className="form-range" type="range" min="1" max="5" name="review[rating]" id="rating" required />
                     </div>
                     <div className="mb-3">
                         <label className="form-label" htmlFor="body">Review</label>
                         <textarea className="form-control" name="review[body]" id="body" cols="30" rows="3" required></textarea>
                         <div className="valid-feedback">
-                    Looks good!
+                            Looks good!
                         </div>
                     </div>
                     <button className="btn btn-success">Submit</button>
                 </form>
+                {campground.review !== [] && campground.review.length > 0 && (
+                    campground.review.map(r => (
+                        <div key={r._id} className="card mb-3 ">
+                            <div className="card-body">
+                                <h5 className="card-title">{`Rating : ${r.rating}`}</h5>
+                                <p className="card-text">{`Review : ${r.body}`}</p>
+                                <form action="/campgrounds/<%=campground._id%>/reviews/<%=review._id%>?_method=DELETE" method="POST">
+                                    <button className="btn btn-sm btn-danger">Delete</button>
+                                </form>
+                            </div>
+                        </div>
+                    ))
+                )}
             </div>
         </div>
 
