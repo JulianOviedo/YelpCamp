a918b4d55d5c570f3d4e56b43440fd79318d72f3
JulianOviedo <julianovie234@gmail.com>
2023-03-13 13:54:06 -0300

    ref margin

--- yelp-front/src/pages/campgrounds/[_id].js
+++ yelp-front/src/pages/campgrounds/[_id].js
@@ -58,7 +58,7 @@ export default function ShowCampground({ _id }) {
                             Looks good!
                         </div>
                     </div>
-                    <button className="btn btn-success">Submit</button>
+                    <button className="btn btn-success mb-3">Submit</button>
                 </form>
                 {campground.review && campground.review.length > 0 && (
                     campground.review.map(r => (
