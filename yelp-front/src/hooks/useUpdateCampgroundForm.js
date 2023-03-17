b2115e3a4239e4184a479ed086938df35208cb29
JulianOviedo <julianovie234@gmail.com>
2023-03-17 10:47:58 -0300

    add hook to handle editForm

---
+++ yelp-front/src/hooks/useUpdateCampgroundForm.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+import { toast } from 'react-hot-toast'
+
+export default function useUpdateCampgroundForm ({ _id, campground }) {
+    const BASE_URL = 'http://localhost:5000'
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        axios.post(`${BASE_URL}/campgrounds/${_id}?_method=PUT`, campground,
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }).then(response => {
+            if (response.status === 200) {
+                toast.success('Campground updated successfully !')
+                window.location.href = `/campgrounds/${response.data.updatedCampgroundId}`
+            }
+        })
+            .catch(err => {
+                const { statusCode, message, stack } = err.response.data
+                toast.error('Ooops ! Something went wrong ... Try again later')
+                window.location.href = `/error?statusCode=${statusCode}&message=${message}&stack=${stack}`
+            })
+    }
+
+    return { handleSubmit }
+}
