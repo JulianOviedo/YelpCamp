31fb723f3d04625bc967feb8723191dd7a2643cf
JulianOviedo <julianovie234@gmail.com>
2023-03-17 11:28:40 -0300

    ref loader

--- yelp-front/src/components/LoadingButton.js
+++ yelp-front/src/components/LoadingButton.js
@@ -1,6 +1,6 @@
 export default function LoadingButton ({ value }) {
     return (
-        <button className="btn btn-primary" type="button" disabled>
+        <button className="btn btn-primary mb-3" type="button" disabled>
             <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
             {value}
         </button>
